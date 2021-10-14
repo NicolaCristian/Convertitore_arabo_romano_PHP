60c8b943f318b7137c3f2509b22ae6fa454ec099
Nicola Cristian <nicola.cristian@osdbsesto>
2021-10-14 13:19:09 +0200

    init commit

---
+++ conv.php
@@ -0,0 +1,50 @@
+<html>
+
+<head> <title>CONVERTITORE ARABO - ROMANO</title></head>
+    <body>
+        <form action="conv.php" method = "get">
+            <p>numero:<input type="number" name="num"></p>
+            <p>romano:<input type="number" name="rom"></p>
+            <input type="submit" values="submit">
+        </form>
+
+
+        <?php
+
+
+$num= $_GET['num'];
+$num_romano=" ";
+while($num>=1000){
+    $num_romano=$num_romano.'M';
+    $num=$num-1000;
+} while($num>=500){
+    $num_romano=$num_romano.'D';
+    $num=$num-500;
+} while($num>=100){
+    $num_romano=$num_romano.'C';
+    $num=$num-100;
+} while($num>=50){
+    $num_romano=$num_romano.'L';
+    $num=$num-50; 
+} while($num>=10){
+    $num_romano=$num_romano.'X';
+    $num=$num-10;
+} while($num>=5){
+    $num_romano=$num_romano.'V';
+    $num=$num-5;    
+} while($num>=1){
+    $num_romano=$num_romano.'I';
+    $num=$num-1;    
+}
+
+echo $num_romano;
+
+
+$rom=$_GET['rom'];
+$num_dec=" ";
+
+
+
+?>
+  </body>
+</html>
